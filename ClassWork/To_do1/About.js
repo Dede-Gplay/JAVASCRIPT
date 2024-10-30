d0837e3b23fb50879865ecefa0bf3434c9d0eab6
Dede-Gplay <dedegodspower556@gmail.com>
2024-10-30 10:13:50 +0100

    work

---
+++ ClassWork/To_do1/About.js
@@ -0,0 +1,24 @@
+// variables in javascript
+let first_name = 'David';
+var last_name = 'John';
+let number = 10;
+let number2 =- 20;
+// let x = x + y;
+let obj = [20, 10, 1, 30, 50, 12, 13, 15];
+
+function putout(){
+    console.log(first_name)
+}
+putout();
+
+function addition(){
+    console.log(first_name + ' ' + 'score' + ' ' + 4 + 5)
+}
+addition()
+function two(a, b, c){
+    return(a+b+c);
+    return(a-b-c)
+    console.log(a+b+c);
+    console.log(a-b-c);
+}
+two(10,5,5)
